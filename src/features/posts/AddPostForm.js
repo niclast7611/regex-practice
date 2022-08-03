1d0ec417ef449ee1c31a15ec3e6d5c6ecd5d63a4
Nic Last <niclast7611@gmail.com>
2022-08-02 18:20:14 -0700

    add thunks and api calls next I will style and add CRUD functionallity and comments to show my understanding

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { postAdded } from "./postsSlice";
+import { addNewPost } from "./postsSlice";
 import { selectAllUsers } from "../users/usersSlice";
 
 const AddPostForm = () => {
@@ -8,6 +8,7 @@ const AddPostForm = () => {
     const [title, setTitle] = useState('')
     const [content, setContent] = useState('')
     const [userId, setUserId] = useState('')
+    const [addRequestStatus, setAddRequestStatus] = useState('idle')
 
     const users = useSelector(selectAllUsers)
 
@@ -15,16 +16,24 @@ const AddPostForm = () => {
     const onContentChanged = e => setContent(e.target.value)
     const onAuthorChanged = e => setUserId(e.target.value)
 
+    const canSave = [title, content, userId].every(Boolean) && addRequestStatus === 'idle'
+
     const onSavePostClicked = () => {
-        if (title && content) {
-            dispatch(
-                postAdded(title, content, userId)
-            )
-            setTitle('')
-            setContent('')
+        if (canSave) {
+            try {
+                setAddRequestStatus('pending')
+                dispatch(addNewPost({ title, body: content, userId })).unwrap()
+
+                setTitle('')
+                setContent('')
+                setUserId('')
+            } catch (err) {
+                console.error('Failed to save the post', err)
+            } finally {
+                setAddRequestStatus('idle')
+            }
         }
     }
-    const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
 
     const usersOptions = users.map(user => (
         <option
@@ -39,7 +48,7 @@ const AddPostForm = () => {
         <section>
             <h2>Add a New Post</h2>
             <form>
-                <label HTMLfor="postTitle">Post Title:</label>
+                <label htmlFor="postTitle">Post Title:</label>
                 <input
                     type="text"
                     id="postTitle"
@@ -47,12 +56,12 @@ const AddPostForm = () => {
                     value={title}
                     onChange={onTitleChanged}
                 />
-                <label HTMLfor="postAuthor">Author:</label>
+                <label htmlFor="postAuthor">Author:</label>
                 <select id="postAuthor" value={userId} onChange={onAuthorChanged}>
                     <option />
                     {usersOptions}
                 </select>
-                <label HTMLfor="postContent">Content:</label>
+                <label htmlFor="postContent">Content:</label>
                 <textarea
                     id="postContent"
                     name="postContent"
