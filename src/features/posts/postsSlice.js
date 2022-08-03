4096edd1e84deeaa1d480a01b6f2fc3cc1303a7c
Nic Last <niclast7611@gmail.com>
2022-08-03 08:35:47 -0700

    added commenting to show my knowledge of the different parts when using redux

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -2,25 +2,33 @@ import { createSlice, nanoid, createAsyncThunk } from "@reduxjs/toolkit";
 import { sub } from 'date-fns';
 import axios from "axios";
 
+//URL for the API call we used
 const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts'
 
+//Initial statefor the posts array is empty because we will push things into it
 const initialState = {
     posts: [],
     status: 'idle', //idle | loading | succeeded | failed
     error: null
 }
 
+//This is the post slice method that contains reducer methods for the posts state
 
+//It accepts initial state a name and reducer functions, and automatically generates actions to correspond with the state
+
+//Immer tech behind the scenes makes it so you dont need to ...
 const postsSlice = createSlice({
     name: 'posts',
     initialState,
     reducers: {
         postAdded: {
+            //Pushing the blog information from the api into a the posts empty array 
             reducer(state, action) {
                 state.posts.push(action.payload)
             },
             prepare(title, content, userId) {
                 return {
+                    //Structuring the data that will be sent to the array. 
                     payload: {
                         id: nanoid(),
                         title,
@@ -38,6 +46,7 @@ const postsSlice = createSlice({
                 }
             }
         },
+        //Another reducer function that adds one to the specific reaction when its clicked on 
         reactionAdded(state, action) {
             const { postId, reaction } = action.payload
             const existingPost = state.posts.find(post => post.id === postId)
@@ -46,15 +55,19 @@ const postsSlice = createSlice({
             }
         }
     },
+    //All this is is assigning values to each part of the loading statge waiting for the promise to get fulfilled
     extraReducers(builder) {
         builder
+            //if the fetchPosts method is pending state changes to loading instead of idle 
             .addCase(fetchPosts.pending, (state) => {
                 state.status = 'loading'
             })
+            //if fulfilled changes state to succeeded and adds more features to the blog post that the api does not have in it like a time stamp and reactions 
             .addCase(fetchPosts.fulfilled, (state, action) => {
                 state.status = 'succeeded'
                 let min = 1
                 const loadedPosts = action.payload.map(post => {
+                    //Mapping over the loadedPosts from the api and adding features mentioned above
                     post.date = sub(new Date(), { minutes: min++ }).toISOString()
                     post.reactions = {
                         thumbsUp: 0,
@@ -65,12 +78,17 @@ const postsSlice = createSlice({
                     }
                     return post
                 })
+                //Because we are in a createSlice we do not need to ... or make a copy fo the state because Immer does it in the background this only works in the createSlice method 
+
+                //So all we are doing is concatting the loadedPosts with the new features into the posts state
                 state.posts = state.posts.concat(loadedPosts)
             })
+            //Has a failure status just incase
             .addCase(fetchPosts.rejected, (state, action) => {
                 state.status = 'failed'
                 state.error = action.error.message
             })
+            //creates a new post through the api formatting
             .addCase(addNewPost.fulfilled, (state, action) => {
                 action.payload.userId = Number(action.payload.userId)
                 action.payload.date = new Date().toISOString()
@@ -86,7 +104,7 @@ const postsSlice = createSlice({
             })
     }
 });
-
+//exporting all of this state, two of the reducer methods and both thunks 
 export const selectAllPosts = (state) => state.posts.posts
 export const getPostsStatus = (state) => state.posts.status
 export const getPostsError = (state) => state.posts.error
